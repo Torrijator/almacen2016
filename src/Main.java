e0541fab6d426a68593298b35ee3108330f6c13a
Ginko <Ginko@TORRIJA>
2016-11-17 12:37:44 +0100

    sdfsf

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub	superchachi	
+		// TODO Auto-generated method stub	
 		Scanner sc	=	new Scanner(System.in);
 		
 		int numero1, numero2, resultado;{
